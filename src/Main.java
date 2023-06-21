cd38a2a386d6faca5c4bd54ecc0d475997604528
svn1311 <svn131@mail.ru>
2023-06-22 01:06:59 +0500

    ноут-vetkaNoutbuka-new-commit1

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,2 @@
 public class Main {
-1
-2
-
-    3
-    vetkaNoutbuka-Commit-1
-    vetkaNoutbuka-Commit-2
-    vetkaNoutbuka-Commit-3
-    vetkaNoutbuka-Commit-5
-
-
-    vetkaPC-komit-1
-    vetkaPC-komit-2
-    5
-    6
-    vetkaPC-posle-merge-komit-1
-    vetkaPC-posle-merge-komit-2
-    vetkaPC-posle-merge2-komit-1
-    vetkaPC-posle-merge2-komit-2
-}
-
 }
