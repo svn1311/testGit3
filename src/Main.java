f1b1d0f34bd0c9a30da20ae9bc13946008734ac2
Vladimir Solovyov <svn131131@gmail.com>
2023-06-22 01:08:47 +0500

    vetkaPC-new-komit-1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,2 @@
 public class Main {
-1
-2
-
-    vetkaPC-komit-1
-    vetkaPC-komit-2
-    5
-    6
-    vetkaPC-posle-merge-komit-1
-    vetkaPC-posle-merge-komit-2
-    vetkaPC-posle-merge2-komit-1
-    vetkaPC-posle-merge2-komit-2
-}
 }
